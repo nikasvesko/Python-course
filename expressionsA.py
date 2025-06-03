e35646aaa8cf654aeb98ff4063b7ee39c09f0159
nikasvesko <nika.svesko@gmail.com>
2025-06-03 17:58:01 +0200

    expressionsA.py

---
+++ expressionsA.py
@@ -0,0 +1,23 @@
+#constants
+print("Hello world")
+
+#reserved words, kao False, if ...
+
+#varijable
+x=12.2
+y=14
+#varijable su promjenjive, stavim opet x, to ce biti nova varijabla
+
+#underscore na pocetku varijable nije bas najbolje
+
+bigmac= 35
+chicken= 12
+mek= 35*12
+print(mek)
+#treba biti mnemonic
+hours=35
+rate=12
+pay=hours*rate
+print(pay) #print statement
+
+#assigment statement zamisliti sa strelicom kao u R, prvo rijesava desnu stranu
