d32b6854b8215afd301ff24a169e3827f4ddcb6e
nikasvesko <nika.svesko@gmail.com>
2025-06-08 00:11:31 +0200

    Update moreconditional.py

--- moreconditional.py
+++ moreconditional.py
@@ -9,4 +9,13 @@ print("all done")
 
 #trebao bi imati else
 
-#the try /except structure
+#the try /except structure, ako try uspije, except se preskace
+
+astr= "hello"
+try:
+  istr=int(astr)
+except:
+  istr = -1
+print("First,istr)
+#ako stavis vise lineova u try ako neki blow upa nece se vratit u try bloc
+      
