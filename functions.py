f763f5f4c334515769b78ef05c99715513171fdc
nikasvesko <nika.svesko@gmail.com>
2025-06-08 16:18:44 +0200

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,19 @@
+#Functions
+def thing(): #stored step, we named it thing, def is reserved word
+  print("hello")
+  print("fun")
+thing() 
+print("Zip")
+
+#built in functions like input, type, float, int...
+
+big= max("hello world")
+print(big) #hello world is a string and output would be "w" which is also a string
+#return value, we have assignment value and we get a result
+
+#a function takes some input and produces an output
+
+#type conversions (int and float)
+
+#string conversions 
+ 
