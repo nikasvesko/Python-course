1c23b3ca6ffe4d3775f1ee24607f476c67c03cd0
nikasvesko <nika.svesko@gmail.com>
2025-06-11 20:42:26 +0200

    Create loopsanditeration.py

---
+++ loopsanditeration.py
@@ -0,0 +1,40 @@
+#repeated steps
+
+n=5
+while n > 0:
+  print(n)
+  n=n-1
+print("blastoff!")
+print(n)
+#dok ide idendentation ide kao if 
+#iteration variables that change each time during loop
+
+#infinite loop je badly constructed
+
+n=0
+while > 0 :
+  print("leather")
+  print("rinse")
+print("dry off")
+#zero trip loop, like if statement
+
+#breaking out of a loop
+#once you do a break, your loop is done
+
+#we have continue as well
+
+while True :
+  line=input(">")
+  if line[0]=="#":
+    continue
+  if line== "done":
+    break
+  print(line)
+print("done!")
+
+#indefinite loops, do breaka ili dok god je nesto tocno
+
+
+
+
+
