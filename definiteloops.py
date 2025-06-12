3f4857468410324a4627cdf14141e2a70081fba0
nikasvesko <nika.svesko@gmail.com>
2025-06-12 23:24:45 +0200

    Create definiteloops.py

---
+++ definiteloops.py
@@ -0,0 +1,14 @@
+#loops through a set of things (finite)
+for i in [5,4,3,2,1]:
+  print(i)
+print("blastoff!")
+#executes 5 times with this values of 5
+
+#definite loop with strings
+friends= ["Joseph", "Sally", "Glenn"]
+for friend in friends:
+  print("happy new year:", friend)
+print("done")
+#its going to print out happy new year: glenn and every name and then done
+
+#initializes and executes
