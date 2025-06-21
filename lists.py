d9278ff0f3f6205d722b958b7036df8b15d0354c
nikasvesko <nika.svesko@gmail.com>
2025-06-21 23:26:55 +0200

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,19 @@
+#algorithms are sets of rules for a computer to solve a problem
+
+#data structures are not steps but ways in which you organize data in a computer
+
+# [] <- for lists
+#it can be asigned into a variable
+
+#list element can be any other python object including another list
+
+for i in [5,4,3,2,1]:
+  print(i)
+print("Blastoff!")
+
+#for friend in friends, uzet ce vrijednosti unutar liste
+#isto se broje od nule, pozicije, kao i strings
+#list are changable
+
+#strings are not
+#range function
