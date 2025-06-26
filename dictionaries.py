f4109d6dbaed4840c5a71c1ff891a4efef02a28a
nikasvesko <nika.svesko@gmail.com>
2025-06-26 17:19:54 +0200

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -1,1 +1,40 @@
+#collection
+#list is organized version of collection and dictionary is messier
+#there is no sense of order
+#things have labels
+#dictionaries are most powerful pythons data collection
+
+#they have different names in different languages
+#like property bag in C#
+
+purse=dict()
+purse['money']=12
+purse['candy']=3
+purse['tissues']=75
+print(purse)
+#sve sto je u purse label i broj
+
+ddd=dict()
+ddd['age']=21
+ddd['course']=182
+print(ddd)
+#sve
+#ako promijenim value agea, isprintat ce taj novi
+
+lst=list()
+lst.append(21)
+lst.append(183)
+print(lst)
+[21,183]
+
+#dictionary literals(constants)
+jjj={'chuck':1,'fred':42,'jan':100}
+print(jjj)
+#isprinta tako pridruzeno
+
+
+
+
+
+
 
